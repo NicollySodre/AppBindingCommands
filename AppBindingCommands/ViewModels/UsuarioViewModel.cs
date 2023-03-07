cf8e78c1e6e06a7aeafa91b2918c72016ea83202
etechas <etechas@LI02-04>
2023-03-06 22:26:09 -0300

    Add project files.

---
+++ AppBindingCommands/ViewModels/UsuarioViewModel.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AppBindingCommands.ViewModels
+{
+    public class UsuarioViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChangeed(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public UsuarioViewModel()
+        {
+            ShowMessageCommand = new Command(ShowMessage);
+            CountCommand = new Command(async() => await CountCharacters());
+            CleanCommand = new Command(async () => await CleanConfirmation());
+            OptionCommand = new Command(async () => await ShowOptions());
+        }
+
+        private string name = string.Empty;//CTRL + R, E
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (name == null) 
+                    return;
+
+                name = value;
+                OnPropertyChangeed(nameof(name));
+                OnPropertyChangeed(nameof(DisplayName));
+            }
+        }
+
+        public string DisplayName => $"Nome digitado: {Name}";
+
+        string displayMessage = string.Empty;
+        public string DisplayMessage { 
+            get => displayMessage; 
+            set 
+            {
+                if (displayMessage == null) 
+                    return;
+
+                displayMessage = value;
+                OnPropertyChangeed(nameof(DisplayMessage));
+            }
+        }
+
+        public ICommand ShowMessageCommand { get; }
+        public void ShowMessage()
+        {
+            DateTime data = Preferences.Get("dtAtual", DateTime.MinValue);
+            DisplayMessage = $"Boa noite {Name}, hoje é {data}";
+        }
+
+        public async Task CountCharacters()
+        {
+            string nameLenght =
+                string.Format("Seu nome tem {0} letras", name.Length);
+
+            await Application.Current
+                .MainPage.DisplayAlert("Informação", nameLenght, "Ok");
+        }
+        public ICommand CountCommand { get; }
+
+        public async Task CleanConfirmation()
+        {
+            if (await Application.Current.MainPage
+                .DisplayAlert("Confirmação", "Confirma limpeza de dados?", "Yes", "No"))
+            {
+                Name = string.Empty;
+                DisplayMessage = string.Empty;
+                OnPropertyChangeed(Name);
+                OnPropertyChangeed(DisplayMessage);
+
+                await Application.Current.MainPage
+                    .DisplayAlert("Informação", "Limpeza realizada com sucesso", "Ok");
+            }
+        }
+        public ICommand CleanCommand { get; }
+
+        public async Task ShowOptions()
+        {
+            string result = await Application.Current.MainPage
+                .DisplayActionSheet("Selecione uma opção: ", "",
+                "Cancelar", "Limpar", "Contar Caracteres", "Exibir Saudação");
+
+            if (result !=null)
+            {
+                if (result.Equals("Limpar"))
+                    await CleanConfirmation();
+                if (result.Equals("Contar Caracteres"))
+                    await CountCharacters();
+                if (result.Equals("Exibir Saudação"))
+                    ShowMessage();
+            }
+        }
+        public ICommand OptionCommand { get; }
+    }
+}
